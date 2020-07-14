44ddb568016996f63b4403ff0d7e56af3524cefe
parwhdgur <jh.park@qualson.com>
2020-07-14 15:45:51 +0900

    feat: add auth server (login)

---
+++ authServer.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const app = express();
+
+const MASTER = {
+  email: "parkoon@gmail.com",
+  password: 1234,
+};
+
+const JWT_SECRET = "supersecret#!@$";
+
+function generateToken(payload) {
+  return jwt.sign(payload, jwt);
+}
+
+app.post("/account/login", (req, res) => {
+  const { email, password } = req.body;
+
+  if (email === MASTER.email && password === MASTER.password) {
+    res.json({
+      token: generateToken({ email }),
+    });
+  }
+
+  res.status(400).json({
+    message: "invalid email or password",
+  });
+});
+
+const PORT = process.env.PORT || 3007;
+app.listen(PORT, () => {
+  console.log("Server running on " + PORT);
+});
