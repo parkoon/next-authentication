1ffe6d8c43327e47c382e5dab61e88608ea8c17a
parwhdgur <jh.park@qualson.com>
2020-07-14 14:44:06 +0900

    feat: add auth context

---
+++ contexts/auth.js
@@ -0,0 +1,63 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import Cookies from "js-cookie";
+import API from "../helpers/api";
+import { apiResolver } from "next/dist/next-server/server/api-utils";
+
+const AuthContext = createContext({});
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function loadUserFromCookies() {
+      const token = Cookies.get("token");
+
+      if (token) {
+        console.log("Got a token in the cookies, lets see if it is valid");
+
+        API.defaults.headers.Authorization = `Bearer ${token}`;
+        const { data } = await API.get("/account/me");
+        if (user) setUser(data.user);
+      }
+
+      setLoading(false);
+    }
+
+    loadUserFromCookies();
+  }, []);
+
+  const login = async (accountName, password) => {
+    const { data } = await API.post("/account/login", {
+      accountName,
+      password,
+    });
+
+    if (data.token) {
+      console.log("Got token");
+      Cookies.set("token", token, { expires: 60 });
+      API.defaults.headers.Authorization = `Bearer ${token}`;
+      const { data: user } = await API.get("/account/me");
+      setUser(user);
+      console.log("Got user", user);
+    }
+  };
+
+  const logout = () => {
+    Cookies.remove("token");
+    setUser(null);
+    window.location.pathname = "/login";
+  };
+  return (
+    <AuthContext.Provider
+      value={{ isAuthenticated: !!user, user, login, loading, logout }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default function useAuth() {
+  const context = useContext(AuthContext);
+  return context;
+}
