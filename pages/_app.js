d1465eb6870827f1bcf814e9f893eb5215b4dd5b
parwhdgur <jh.park@qualson.com>
2020-07-14 14:46:16 +0900

    feat: wrapping AuthProvider

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
+import { AuthProvider } from "../contexts/auth";
+
 function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />;
+    </AuthProvider>
+  );
 }
 
 export default App;
