4de40ea01d6ee94417e5ef691dfd69effc7408ad
parwhdgur <jh.park@qualson.com>
2020-07-14 14:57:19 +0900

    feat: add todo page

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,25 @@
 import { AuthProvider } from "../contexts/auth";
 
-function App({ Component, pageProps }) {
+// import App from 'next/app'
+
+function MyApp({ Component, pageProps }) {
   return (
     <AuthProvider>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </AuthProvider>
   );
 }
 
-export default App;
+// Only uncomment this method if you have blocking data requirements for
+// every single page in your application. This disables the ability to
+// perform automatic static optimization, causing every page in your app to
+// be server-side rendered.
+//
+// MyApp.getInitialProps = async (appContext) => {
+//   // calls page's `getInitialProps` and fills `appProps.pageProps`
+//   const appProps = await App.getInitialProps(appContext);
+//
+//   return { ...appProps }
+// }
+
+export default MyApp;
---
+++ pages/todo.js
@@ -0,0 +1,10 @@
+function Todo() {
+  return (
+    <>
+      <h1>Todo Page</h1>
+      <p>this page is protected!</p>
+    </>
+  );
+}
+
+export default Todo;
